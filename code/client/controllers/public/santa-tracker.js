ad72ebd31a206d848f63ab46b799ea6b58fd3d14
Ryan Glover <me@ryanglover.net>
2014-12-10 09:46:27 -0600

    Clean up interface a bit. Add styles for sleigh connection status. Fix map so that it locks its zoom.

--- code/client/controllers/public/santa-tracker.js
+++ code/client/controllers/public/santa-tracker.js
@@ -2,7 +2,11 @@ Template.santaTracker.rendered = function(){
   Tracker.autorun(function () {
     if (Mapbox.loaded()) {
       L.mapbox.accessToken = "pk.eyJ1IjoidGhlbWV0ZW9yY2hlZiIsImEiOiJmelotbTdrIn0.QmNnWntI9zqeqLrxS6mRkA";
-      var map = L.mapbox.map('map', 'themeteorchef.kdnbci0g');
+      L.mapbox.map('map', 'themeteorchef.450d4794', {
+        zoom: 3,
+        minZoom: 3,
+        maxZoom: 6
+      });
     }
   });
 }
